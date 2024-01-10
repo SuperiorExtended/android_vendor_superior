PixelExperience/vendor_aosp

vendor: Import clocks package
Change-Id: I0025fe6f12d68af8f4a8f4b256bd82822d37efdf

config/common.mk
@@ -219,6 +219,9 @@ $(call inherit-product, vendor/gms/products/gms.mk)
 # RRO Overlays
 $(call inherit-product, vendor/aosp/config/rro_overlays.mk)
 
+# Clocks
+$(call inherit-product, vendor/aosp/config/clocks.mk)
+
 # Pixel Framework
 #$(call inherit-product, vendor/pixel-framework/config.mk)
 

SuperiorExtended/android_vendor_superior

Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

config/common.mk
@@ -53,6 +53,9 @@ ifneq ($(TARGET_BUILD_VARIANT),user)
 PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/bin/procmem
 endif
+    
+# Clocks
+$(call inherit-product, vendor/superior/config/clocks.mk)
 
 # Protobuf - Workaround for prebuilt Qualcomm HAL
 PRODUCT_PACKAGES += \