f8d7e50323462469aaa407141304f9ef2ae61a65
S I P U N <sipunkumar85@gmail.com>
2024-01-25 07:10:04 +0000

    vendor: Fix roomservice
    - Update remote for devices org
    - Update default revision
    
    Change-Id: I7b70e61797dc1d3a894349db11b362488830f2aa
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- build/tools/roomservice.py
+++ build/tools/roomservice.py
@@ -30,11 +30,11 @@ import urllib.request
 DEBUG = False
 
 custom_local_manifest = ".repo/local_manifests/superior.xml"
-custom_default_revision = "fourteen"
-custom_default_revision =  os.getenv('ROOMSERVICE_DEFAULT_BRANCH', 'fourteen')
+custom_default_revision = "UDC"
+custom_default_revision =  os.getenv('ROOMSERVICE_DEFAULT_BRANCH', 'UDC')
 custom_dependencies = "superior.dependencies"
-org_manifest = "SuperiorOS-Devices"  # leave empty if org is provided in manifest
-org_display = "SuperiorOS-Devices"  # needed for displaying
+org_manifest = "Extended-Devices"  # leave empty if org is provided in manifest
+org_display = "SuperiorExtended-Devices"  # needed for displaying
 
 github_auth = None
 
